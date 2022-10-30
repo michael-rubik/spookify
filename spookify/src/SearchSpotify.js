6ec072e17b6e8d3befa03679eafe9e6effc0e411
ChillySpiral <81975828+ChillySpiral@users.noreply.github.com>
2022-10-30 18:42:23 +0100

    Fix

--- spookify/src/SearchSpotify.js
+++ spookify/src/SearchSpotify.js
@@ -1,60 +1,30 @@
 import React, {Component} from 'react';
 import SpotifyWebApi from 'spotify-web-api-js';
-import axios from 'axios';
-import qs from 'qs';
 
 class SearchSpotify extends Component{
     static CLIENT_ID = "2c7bd7c1873e49d5afb4f45d46de8ea3";
     static CLIENT_SECRET = "45b68b79d1b04e0e98b965fb000ef276";
 
-    static token = null;
-
+    
     state = { query: "" }
 
     accessSpotifyAPI = async (query) => {
         const spotify = new SpotifyWebApi();
-        
-        const headers = {
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            auth: {
-                username: "----", //Client ID
-                password: "----", //Client Secret
-            },
-            };
-
-        const data = {
-        grant_type: 'client_credentials',
-        };
-
-        try {
-        const response = await axios.post(
-            'https://accounts.spotify.com/api/token',
-            qs.stringify(data),
-            headers
-        );
-
-        console.log(response.data.access_token);
-        let token = response.data.access_token;
+        let token = "----"; //User Token from https://developer.spotify.com/console/post-playlist-tracks/
 
         spotify.setAccessToken(token)
         spotify.searchTracks(query, { limit: 1 }).then(
             (data) => {
                 console.log("Search results", data.tracks.items)
-                alert(data.tracks.items[0].name + " by " + data.tracks.items[0].artists[0].name)
+                alert(data.tracks.items[0].name + " by " + data.tracks.items[0].artists[0].name);
+                spotify.addTracksToPlaylist("----", ["spotify:track:"+data.tracks.items[0].id.toString()]); //Playlist ID
             },
             (err) => {
             console.error(err);
             }
 
         )} 
-        catch (error) {
-        console.log(error);
-        }
-      }
-
+      
       handleChange = (e) => this.setState({ query: e.target.value })
       handleSubmit = (e) => {
         e.preventDefault();        
