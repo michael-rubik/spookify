ba747a821ca203be912610a96fc7f550797e885b
ChillySpiral <81975828+ChillySpiral@users.noreply.github.com>
2022-10-30 18:02:35 +0100

    Added React app including component to search

---
+++ spookify/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import SearchSpotify from './SearchSpotify';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SearchSpotify />
+      </header>
+    </div>
+  );
+}
+
+export default App;
